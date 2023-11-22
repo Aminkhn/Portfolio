f3ff434c36ca70df6ab82ddfcea0fa1e182cf3c7
amin <aminkhalilinasab@gmail.com>
2023-11-22 22:21:22 +0330

    view page handler created

--- handler/pageHandler/page.go
+++ handler/pageHandler/page.go
@@ -4,6 +4,6 @@ import "github.com/gofiber/fiber/v2"
 
 func Home(c *fiber.Ctx) error {
 	return c.Render("index", fiber.Map{
-		"Title": "welcome to our portfolio website (AMIN & MOHAMMAD)",
-	})
+		"Title": "welcome I am Amin & Mohammad",
+	}, "layouts/main")
 }
